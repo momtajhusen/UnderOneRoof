e1f2ca781569fbcd9c324d56a7fdbe6c9001c5e9
momtajhusen <mumtaz666raza@gmail.com>
2025-06-14 13:26:36 +0530

    Fix filename casing issue for entitlements file

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { View, StatusBar, Platform } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './navigation/StackNavigation';
 import { AppProvider } from './context/AppContext';
 import { MenuProvider } from 'react-native-popup-menu';
 
-const App = () => {
-  return (
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets
+} from 'react-native-safe-area-context';
+
+const App = () => (
+  <SafeAreaProvider>
+    <StatusBar
+      barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+      backgroundColor="transparent"
+      translucent
+    />
     <MenuProvider>
-    <AppProvider>
-      <NavigationContainer>
-        <StackNavigation />
-      </NavigationContainer>
-    </AppProvider>
+      <AppProvider>
+        <NavigationContainer>
+          <StackNavigation />
+        </NavigationContainer>
+      </AppProvider>
     </MenuProvider>
-  );
-};
+  </SafeAreaProvider>
+);
 
 export default App;
